41e1012565caea104bf952f52566aeb4c9930cb5
Addison Beyer <addisonbeyer@gmail.com>
2022-07-03 22:52:09 -0500

    Initial commit

---
+++ src/main/java/se/citerus/dddsample/DddsampleApplication.java
@@ -0,0 +1,13 @@
+package se.citerus.dddsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddsampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/citerus/dddsample/DddsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package se.citerus.dddsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
